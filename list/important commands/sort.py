ebc7cc98135cffb1981f9e532064a1f410786c85
Alireza Doustmohammadi <a.doustmohammadi@modares.ac.ir>
2022-07-21 12:56:01 +0430

    sort a list

---
+++ list/important commands/sort.py
@@ -0,0 +1,31 @@
+
+list1 = []
+list2 = [6,5,4,8,1]
+
+# add to list
+# element to list
+# list to list
+
+
+
+# list1 = []
+list1.append(2)
+# list1 = [2]
+list1.append(3)
+# list1 = [2, 3]
+#print(list1)
+list1.insert(1, 5)
+#list1.append([2,3])
+list1.extend(list2)
+print(list1)
+#print(list1[-1])
+#list1_unique = list(set(list1))
+#print(list1_unique)
+list1 = sorted(list1 , reverse = True)
+print(list1)
+
+print(list1[-1])
+print(list1[0:2])
+
+
+
